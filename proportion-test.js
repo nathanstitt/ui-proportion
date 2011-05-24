429b91d4ab88f9e8dc14cc0b0986979378ebf163
Nathan Stitt <nathan@stitt.org>
2011-05-24 16:07:38 -0500

    working with two labels/one slider now

--- proportion-test.js
+++ proportion-test.js
@@ -2,39 +2,63 @@
 $(document).ready(function(){
 
         $(document).ready(function(){
-
-                var el = $('#slider').proportion({
-                        max: 442.32,
-                        labels: [
-                                { name: 'One', value: 100 }
-
-                        ]
-                });
+                function round10th( num ){
+                        return Math.round( 10 *  num ) / 10;
+                };
+                var make_slider = function(){
+                        return $('#slider').proportion({
+                                max: 442.32,
+                                labels: [
+                                        { name: 'One', value: 100 }
+                                ]
+                        });;
+                };
                 
                 test("has a slider", function() {
+                        var el=make_slider();
                         ok( el.hasClass('ui-slider'), "initialized a slider" );
                 });
-
-                test("only creates one slider, with one label", function() {
-                        var slider = $('#slider').proportion(),
-                            label = $('.ui-slider .ui-proportion-label');
-
-                        equals( 1, $('.ui-slider').length );
-                        equals( 1, label.length );
-                        equals( 2, parseInt( 100 * ( slider.width() / label.position().left ) ) / 100 );
-                });
-                
                 test("Can be destroyed", function() {
-                        var slider = $('#slider').proportion();
-
-                        equals( 1, $('.ui-slider').length );
+                        var el=make_slider();
+                        equals( $('.ui-slider').length, 1, 'created a single slider' );
 
                         $('#slider').proportion('destroy');
-                        
-                        equals( 1, $('#slider').length,'Slider div still remains' );
-                        equals( 0, $('.ui-slider').length );
-                        equals( 0, $('.ui-proportion-label').length );
+
+                        equals( $('#slider').length,1, 'Slider div still remains' );
+                        equals( $('.ui-slider').length, 0, 'Has no sliders'  );
+                        equals( $('.ui-proportion-label').length,0,'Has no labels' );
                 });
+                test("only creates one slider, with one label", function() {
+                        var slider=make_slider(),
+                            label = $('.ui-slider .ui-proportion-label');
+
+                        equals( $('.ui-slider').length, 1, 'only one slider was created');
+                        equals( label.length, 1, 'with one label' );
+                        equals( round10th(slider.width() / label.position().left), 2.0, 'Label positioned in middle');
+                });
+
+                test("creates multiple handles & labels", function(){
+                        
+                        var opts={
+                                max: 500,
+                                labels: [
+                                        { name: 'First', value: 200 },
+                                        { name: 'Second', value: 300 }
+                                ] },
+                            slider=$('#slider').proportion( opts ),
+                            labels = $('.ui-slider .ui-proportion-label');
+
+                        equals( 2, $('.ui-slider .ui-proportion-label').length, 'Has two labels' );
+                
+                                
+                        equals( round10th( $(labels[0]).position().left / slider.width() ),
+                                ( opts.labels[0].value/opts.max ) / 2,
+                                'First label positioned correctly');
+                        
+
+                });
+                
+
         });
         
 });
--- proportion.js
+++ proportion.js
@@ -6,13 +6,16 @@
                 
         };
         Label.prototype.render = function(){
-                this.el = $('<div class="ui-widget-content ui-proportion-label" style="left: "' +
+                this.el = $('<div class="ui-widget-content ui-proportion-label" style="left:' +
                             this.position + '%"><p class="ui-label">' + this.name + '</p></div>');
                 return this;
         };
         Label.prototype.setPosition = function( pos ){
                 this.position = pos;
-                this.render();
+                if ( ! this.el ){
+                        this.render();
+                }
+                this.el.css('left', pos + '%' );
                 return this;
         };
         Label.prototype.destroy = function(){
@@ -22,53 +25,70 @@
         
         var Slider = function(el, opts ){
                 this.el = el;
+                this.labels = [];
                 this.options = opts;
-                if ( this.options.labels.length ) {
-                        this.setLabels( this.options.labels );
-                }
+                this.setLabels( this.options.labels || [] );
+
         };
+        Slider.prototype.onSlide = function(){
+                var ttl = 0, pos,handle;
+                for ( var i=0, handles=this.el.find('.ui-slider-handle'), len=handles.length; i< len; i++){
+                        handle = $(handles[i]);
+                        pos = parseInt(handle.css('left'));
+                        ttl += pos;
+                        var perc = ttl - ( pos/2 );
+//                        console.log("Setting " + i + " pos: " + pos + ' perc: ' + perc );
+                        this.labels[i].setPosition(  perc );
+//                        console.log( "Total: " + ttl );
+                }
+                pos = ttl + ( ( 100 - ttl ) / 2 );
+//                console.log( "Last set to: " + pos );
+                this.labels[i].setPosition( pos );
+        };
+
         Slider.prototype.setLabels = function( new_labels ){
+                this.destroy();
                 this.labels = [];
                 var handles=[],
-                    qty = 0,
-                name, width;
+                    ttl = 0,
+                    scaleFactor = 100 / this.options.max,
+                    name, width;
                 
-                for ( var i=0,len=new_labels.length; i < len; ++i ){
+                for ( var i=0,len=new_labels.length; i < len; i++ ){
+
                         if ('object' == typeof( new_labels[i] ) ){
                                 name  = new_labels[i].name;
-                                width = new_labels[i].value;
+                                width = new_labels[i].value*scaleFactor;
                         } else {
                                 name = new_labels[i];
-                                width = ( this.options.max / len ) * i;
+                                width = 100 / len * i;
                         }
 
-                        qty += width;
-                                
                         var label = new Label( new_labels[i] );
-                        
-                        if ( this.labels.length ){
-                                handles.push( qty );
-                                label.setPosition( this.options.max / ( qty - (width/2) ) );
-                        } else {
-                                label.setPosition( 50 );
-                        }
 
+//                        console.log( name + ' width: ' + width + ' ttl: ' + ttl );
+                        if ( this.labels.length ){
+                                handles.push( ttl );
+                        }
+                        ttl += width;
                         this.labels.push( label );
                         this.el.append( label.render().el );
                 }
-
-                if ( handles.lenth ){
+                
+                if ( handles.length ){
                         this.el.slider({
                                 values: handles,
-                                max: this.options.max
+                                slide: jQuery.proxy(this.onSlide,this)
                         });
                 } else {
-                        this.el.slider( {max: this.options.max } ).find('.ui-slider-handle').remove();
+                        this.el.slider().find('.ui-slider-handle').remove();
+                        
                 }
-
+                this.onSlide();
                 return this;
         };
         Slider.prototype.destroy = function(){
+                
                 for ( var i=0,len=this.labels.length; i < len; ++i ){
                         this.labels[i].destroy();
                 }
@@ -76,7 +96,8 @@
                 return this;
         };
         
-        $.fn.proportion = function( options ) {
+    $.fn.proportion = function( options ) {
+        
                 var args     = arguments,
                     defaults = {
                         'max'         : 100
@@ -87,17 +108,25 @@
                 } else {
                         options = defaults;
                 }
-
+                
                 return this.each(function() {
                         var $this = $(this),
                             obj = $this.data('proportion');
-                        if ( ! obj ){
+
+                        if ( ! args.length || 'object' == typeof( args[0] ) ) {
+                                if ( obj ){
+                                        obj.destroy();
+                                }
                                 obj = new Slider( $this, options );
                                 $(this).data('proportion',obj);
-                        } else if ( obj[options] ) {
-                                return obj[options].apply( obj, Array.prototype.slice.call( args, 1 ) );
-                        } else if ( args.length ){
-                                $.error( 'Method ' +  args[0] + ' does not exist on jQuery.proportion' );
+                        } else if ( args.length && 'string' == typeof(args[0]) ){
+                                if ( obj && obj[ args[0] ] ) {
+                                        return obj[ args[0] ].apply( obj, Array.prototype.slice.call( args, 1 ) );
+                                } else {
+                                        $.error( 'Method ' +  args[0] + ' does not exist on jQuery.proportion' );
+                                }
+                        } else {
+                                $.error( 'invalid arguments to initialize jQuery.proportion' );
                         }
                         return this;
                 });
